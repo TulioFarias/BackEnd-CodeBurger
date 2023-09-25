0db2b9135b85d29d77b4dc2c40c81ee75865bdb8
TulioFarias <tuliott10@gmail.com>
2023-09-25 15:09:00 -0300

    alterando cors

--- src/app.js
+++ src/app.js
@@ -4,10 +4,15 @@ import {resolve} from 'path'
 import './database'
 import cors from 'cors'
 
+const corsOptions = {
+  origin: 'https://front-end-code-burger.vercel.app',
+  credentials: true,
+}
+
 class App {
   constructor() {
     this.app = express();
-    this.app.use(cors());
+    this.app.use(cors(corsOptions));
 
     this.middlewares();
     this.routes();
